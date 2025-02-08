3e4c815eb8c32ac82c16f981c3fdd1e3eb01851e
Jon Dehdari <jon@dehdari.org>
2025-02-07 20:59:07 -0800

    +llama-zenity.sh; +Llama-Zenity.desktop

---
+++ llama-zenity.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# By Jon Dehdari, 2025
+# Simple GUI to invoke Llama-server
+# For example, you can create a desktop icon that invokes the command: /home/User/src/llama-zenity/llama-zenity.sh '/home/User/src/llama.cpp/build/bin/'
+
+PATH=$PATH:$1
+MODEL_DIR=$HOME/models/
+
+GGUF_FILE=`zenity --title='Select GGUF Model File' --text 'Select GGUF Model File' --file-selection --filename="$MODEL_DIR"  --file-filter='*.gguf'`
+TEMP=`zenity --title='Select Temperature' --text 'Select Temperature (0 is more deterministic and less creative)' --entry --entry-text=0.0`
+NGL=`zenity --title='Select Number of GPU Layers' --text 'Select Number of GPU Layers (0 if you have no GPU)' --scale --value=65 --min-value=0 --max-value=100`
+
+path_to_llama_server=$(which llama-server)
+if ! [ -x "$path_to_llama_server" ]; then
+	path_to_llama_server=`zenity --title='Select Llama-server Path' --text 'Select Llama-server Path. You should probably add this to your $PATH'  --file-selection`
+fi
+
+xdg-open http://localhost:8080
+$path_to_llama_server -ngl $NGL --temp $TEMP --model $GGUF_FILE
